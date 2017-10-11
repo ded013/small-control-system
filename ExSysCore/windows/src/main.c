d0d324b1621abab932ec19aecb67dfdbc57c912e
Ded013 <Ded013@Lenovo68>
2017-10-11 15:01:55 +0300

    Test

--- ExSysCore/windows/src/main.c
+++ ExSysCore/windows/src/main.c
@@ -17,6 +17,7 @@
 #include "config.h"
 #include "tag_types.h"
 
+#include "espdevice_tag_types.h"
 #include "mqtt_tag_types.h"
 #include "rf_tag_types.h"
 
@@ -70,6 +71,7 @@ static void stopExecutionTask()
 
 static void tag_types_initialization()
 {
+	espdevice_type_initialization();
 	mqtt_type_initialization();
 	rf_sensor_type_initialization();
 #ifdef ADDER_TYPE
@@ -93,32 +95,51 @@ static void main_menu_help()
 	printf("v   - set value to tag\r\n");
 	printf("s   - execute one cycle\r\n");
 	printf("r   - run periodic execute\r\n");
-	printf("p   - pause periodic execure\r\n");
+	printf("p   - pause periodic execute\r\n");
+    printf("lf  - set link imitation mode\r\n");
+    printf("lr  - reset link imitation mode\r\n");
 
 	printf("h   - get help info\r\n");
 	printf("x   - exit programm\r\n");
 }
 
 int main(void) {
-    FILE *input = NULL;
+    FILE * cfg_file = NULL;
+    FILE * flash_file = NULL;
 	USERCFG config_buf;
-	DEVICE_CFG deviceCfg;
 
-    input = fopen("D:/DesktopProjects/IoTGateway/config.bin", "rb");
-    if (input == NULL) {
+    cfg_file = fopen("D:/DesktopProjects/IoTGateway/config.bin", "rb");
+    if (cfg_file == NULL) {
         printf("Error opening file");
         exit(ERROR_FILE_OPEN);
     }
 
-    fseek (input, 0, SEEK_END);
-    int fsize = ftell (input);
-    fseek(input, 0, SEEK_SET);
-    fread(&config_buf, fsize, 1, input);
-    fclose(input);
+    fseek (cfg_file, 0, SEEK_END);
+    int fsize = ftell (cfg_file);
+    fseek(cfg_file, 0, SEEK_SET);
+    fread(&config_buf, fsize, 1, cfg_file);
+    fclose(cfg_file);
+
+    uint16_t flash_size_block;
+    void * flash_block;
+    flash_file = fopen("D:/DesktopProjects/IoTGateway/flash_save.bin", "rb");
+    if (flash_file != NULL)
+    {
+    	fseek (flash_file, 0, SEEK_END);
+    	fsize = ftell (flash_file);
+    	uint8_t buff[4096];
+    	fseek(flash_file, 0, SEEK_SET);
+    	fread(buff, fsize, 1, flash_file);
+    	if (!set_saved_block(fsize, buff)) init_saved_block();
+    }
+    else
+    	init_saved_block();
+    fclose(flash_file);
+
 
     init_time_counter();
     tag_types_initialization();
-    CFG_Load(config_buf, &deviceCfg);
+    CFG_Load(&config_buf);
     execute_chain * main_chain = get_main_chain();
 
     char	cmd_str[5];
@@ -136,7 +157,14 @@ int main(void) {
     		main_menu_help();
     	}
     	if (strcmp(cmd_str, "x") == 0) break;
-    	if (strcmp(cmd_str, "c") == 0)  CFG_Print(&deviceCfg);
+    	if (strcmp(cmd_str, "c") == 0)
+    	{
+    		espdevice_config * dev_cfg = get_espdevice_config();
+    		if (!dev_cfg)
+    			INFO("Not find config info.\r\n");
+    		else
+    			CFG_Print(dev_cfg);
+    	}
     	if (strcmp(cmd_str, "t") == 0)
     	{
           printf("Count tags = %d\r\n", main_chain->count_tags);
@@ -173,6 +201,28 @@ int main(void) {
     		else
     			printf("Not find tag with id:%d.\r\n", _id);
     	}
+    	if (strcmp(cmd_str, "lf") == 0)
+    	{
+    		int _id = 0;
+    		printf("\r\nGet link ID:");
+    		scanf("%d",&_id);
+			printf("\r\nGet input mean:");
+			scanf("%s",cmd_str);
+			if (link_forced_write(_id, cmd_str))
+				printf("Set forced mode.\r\n");
+			else
+				printf("Not set forced mode.\r\n");
+    	}
+    	if (strcmp(cmd_str, "lr") == 0)
+    	{
+    		int _id = 0;
+    		printf("\r\nGet link ID:");
+    		scanf("%d",&_id);
+			if (link_forced_reset(_id))
+				printf("Reset forced mode.\r\n");
+			else
+				printf("Operation not complete.\r\n");
+    	}
     	if (strcmp(cmd_str, "s") == 0)
     	{
     		tags_execution();
@@ -199,5 +249,13 @@ int main(void) {
     	}
     }
 
+    flash_file = fopen("D:/DesktopProjects/IoTGateway/flash_save.bin", "wb");
+    if (flash_file == NULL) {
+        printf("Error opening flash file");
+        exit(ERROR_FILE_OPEN);
+    }
+    get_saved_block(&flash_size_block, &flash_block);
+    fwrite(flash_block, 1, flash_size_block, flash_file);
+    fclose(flash_file);
 	return EXIT_SUCCESS;
 }
